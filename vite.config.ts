5c99c62bee39d73c1787fb7de4c9e10c5438a023
raventu <80778915raventu@gmail.com>
2024-07-19 10:24:50 +0800

    init: init repo

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import monkey, { util } from 'vite-plugin-monkey';
+import AutoImport from 'unplugin-auto-import/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    AutoImport({
+      imports: [util.unimportPreset],
+    }),
+    monkey({
+      entry: 'src/main.ts',
+      userscript: {
+        icon: 'https://vitejs.dev/logo.svg',
+        namespace: 'npm/vite-plugin-monkey',
+        match: ['https://www.google.com/'],
+      },
+    }),
+  ],
+});
